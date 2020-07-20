765f36b6ebced17d0d9f95ea423d3e48dc977e3e
linaurena <lina8maria8@hotmail.com>
2020-07-20 18:19:42 -0500

    Clase 1 Variables

--- clase1.js
+++ clase1.js
@@ -1,9 +1,54 @@
+//Esta es mi primera línea//
+console.log('Hola mundo JavaScript')
+
+//Bloque de ejemplo//
 var nombre = 'Sacha', apellido = 'Lifszyc'
 var edad = 28
 
 edad = '28 años'
 
 console.log('Hola ' + nombre + ' ' + apellido)
-console.log('Tengo ' + edad)
+console.log('Sacha tiene ' + edad)
 
 var peso = 75
+
+//Práctica//
+var nombre
+nombre = 'Lina'
+
+console.log('Hola ' + nombre)
+
+//Es igual que escribir lo siguiente//
+var nombre = 'Lina'
+
+console.log('Hola ' + nombre)
+
+//Ahora se declara la variable apellido//
+var nombre = 'Lina'
+var apellido = 'Ureña'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+
+//También se pueden declarar variables en una misma línea separándolas por una coma
+var nombre = 'Lina', apellido = 'Ureña'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+
+//Las variables pueden ser de tipo string o número
+var edad = 22
+
+console.log('Tengo ' + edad + ' años')
+
+//Ó//
+var edad = 22
+
+edad = '22 años'
+
+console.log('Tengo ' + edad)
+
+//Las variables se pueden modificar de tipo//
+var peso = 56
+
+peso = '56 Kg'
+
+console.log('Actualmente peso ' + peso)
