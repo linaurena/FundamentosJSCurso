206e4b5fe15828e3d95790f33a9410aefb4b58d6
linaurena <lina8maria8@hotmail.com>
2020-07-21 16:04:43 -0500

    Second class

--- class2.js
+++ class2.js
@@ -1,3 +1,4 @@
+//Bloque de ejemplo//
 var nombre = 'Sacha', apellido = 'Lifszyc'
 
 var nombreEnMayusculas = nombre.toUpperCase()
@@ -9,3 +10,24 @@ var cantidadDeLetrasDelNombre = nombre.length
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
 
 var str = nombre.substr(1, 2)
+
+//Mi bloque de código//
+var nombre = 'Lina', apellido = 'Ureña'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0) //charAt es la posición del caracter al que queremos acceder
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = nombre + ' ' + apellido //Concatenar cadenas de texto
+var nombreCompletoInterpolado = `${nombre} ${apellido}` //Interpolación de texto
+
+//También se pueden incluir propiedades al interpolar
+var nombrePersonaje = 'Homero', apellidoPersonaje = 'Simpson'
+
+var nombreCompletoPersonajeRandom = `${nombrePersonaje} ${apellidoPersonaje.toUpperCase()}`
+
+//Para acceder a un string dentro de otro string//
+var str = nombre.charAt(1) + nombre.charAt(2)
+var string = nombre.substr(1,2)
